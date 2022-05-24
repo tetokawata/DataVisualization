70def5453d15081465bc9b49bc82cc896f135721
川田恵介 <keisukekawata@iss.u-tokyo.ac.jp>
2022-05-24 20:54:29 +0900

    Set

--- CodeForSlide7.R
+++ CodeForSlide7.R
@@ -4,23 +4,39 @@
 library(tidyverse) # Data import/Hundling/Visualization
 library(estimatr) # Summary statistics
 library(MatchIt) # Tree
+library(gtsummary)
 
 # Data ----
 
 raw <- read_csv("example.csv") # Import data
 
-# Matching ----
+# Matching on Average treatment effect ----
 
 m.out <- matchit(dfmfd ~ agehead + dmmfd + educhead + sexhead,
   data = raw,
-  method = "cem"
-) # CEM
+  method = "cem") # CEM
 
 m.out # Check matching results
 
 plot(summary(m.out), xlim = c(0, 0.4))
 
-# Regression after matching ----
+matched.data <- match.data(m.out,
+                           drop.unmatched = FALSE)
+
+matched.data <- mutate(matched.data,
+                       drop = if_else(is.na(subclass),"drop","not"))
+
+tbl_summary(select(matched.data,
+                   agehead,
+                   dmmfd,
+                   educhead,
+                   sexhead,
+                   drop),
+            by = drop)
+
+## Regression after matching ----
+
+
 
 fit <- lm_robust(exptot ~ dfmfd + agehead + dmmfd + educhead + sexhead,
   data = match.data(m.out),
@@ -39,3 +55,36 @@ ggplot(fit, aes(
   y = method
 )) +
   geom_pointrange()
+
+
+# PS match on Average treatment effect ----
+
+m.out <- matchit(dfmfd ~ agehead + dmmfd + educhead + sexhead,
+                 data = raw,
+                 method = "subclass") # CEM
+
+m.out # Check matching results
+
+plot(summary(m.out), xlim = c(0, 0.4))
+
+
+## Regression after matching ----
+
+fit <- lm_robust(exptot ~ dfmfd + agehead + dmmfd + educhead + sexhead,
+                 data = match.data(m.out),
+                 weights = weights,
+                 clusters = subclass
+)
+
+fit <- tidy(fit)
+fit <- filter(fit, term == "dfmfd")
+fit <- mutate(fit, method = "Matching")
+
+ggplot(fit, aes(
+  x = estimate,
+  xmin = conf.low,
+  xmax = conf.high,
+  y = method
+)) +
+  geom_pointrange()
+
